67fd63351152782fbce218cdc9622a5b5720c5f0
Wasim01s <nomanalam123421@gmail.com>
2026-06-04 19:59:30 +0530

    Update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'myapp',
+  appName: 'Cricket Calculator Pro',
   webDir: 'www'
 };
 
